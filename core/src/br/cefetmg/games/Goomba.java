bb0db39c2b6c3598a366a8496352f2e50c313640
Andre Brait <andrebrait@gmail.com>
2017-08-18 00:04:46 -0300

    Exercício 5 - Animação

--- core/src/br/cefetmg/games/Goomba.java
+++ core/src/br/cefetmg/games/Goomba.java
@@ -3,22 +3,31 @@ package br.cefetmg.games;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Goomba {
 
 	private float goombaSpeed = 20.0f;
-	private float goombaX, goombaY;
+	private float goombaX, goombaY, animationTime;
 
-	private Sprite goombaSprite;
+	private Animation<TextureRegion> goombaAnimation;
+
+	int width, height;
 
 	public Goomba(Texture goombaTexture) {
-		this.goombaSprite = new Sprite(goombaTexture);
+		this.width = 21;
+		this.height = 24;
+		TextureRegion[][] tr = TextureRegion.split(goombaTexture, width, height);
+		this.goombaAnimation = new Animation<TextureRegion>(0.1f, tr[0]);
+		this.goombaAnimation.setPlayMode(PlayMode.LOOP_PINGPONG);
+		this.animationTime = 0.0f;
 	}
 
 	public void render(SpriteBatch batch) {
-		batch.draw(goombaSprite, (int) goombaX, (int) goombaY);
+		batch.draw(goombaAnimation.getKeyFrame(animationTime), (int) goombaX, (int) goombaY);
 	}
 
 	public void update(float delta, Texture map) {
@@ -39,12 +48,14 @@ public class Goomba {
 
 		float newGoombaX = goombaX + deltaX;
 		float newGoombaY = goombaY + deltaY;
-		if (newGoombaX >= 0.0f && newGoombaX <= map.getWidth() - goombaSprite.getWidth()) {
+		if (newGoombaX >= 0.0f && newGoombaX <= map.getWidth() - width) {
 			goombaX = newGoombaX;
 		}
-		if (newGoombaY >= 0.0f && newGoombaY <= map.getHeight() - goombaSprite.getHeight()) {
+		if (newGoombaY >= 0.0f && newGoombaY <= map.getHeight() - height) {
 			goombaY = newGoombaY;
 		}
+
+		animationTime += delta;
 	}
 
 }
